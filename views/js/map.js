6e1985e000875b053ab06a934a793c89b26e7a90
Kyriakos <stylianou98@gmail.com>
2023-05-13 23:25:41 +0300

    up

--- views/js/map.js
+++ views/js/map.js
@@ -1,7 +1,8 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3N0eWxpYW5vdSIsImEiOiJja2w4OWdlY3owZHFqMndydnlqYWdwODhzIn0.5p3nIkXRh8PeBiM-caYVJQ';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    //style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
     center: [34.054960,35.015934],
     zoom: 15,
 });
